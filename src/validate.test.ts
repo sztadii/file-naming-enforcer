993a7f11cca0e94f9e188f21f469f51dcd9d6f0f
Krystian Sztadhaus <sztadii@gmail.com>
2019-08-26 11:43:32 +0400

    Prepare working MVP for validate function and test it

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -7,12 +7,52 @@ function mockArgv(args: string[]) {
   process.argv = [...oldArgs, ...args]
 }
 
-test('validate function just call console.log', async () => {
-  const args = ['type=kebabCase', 'path=./', 'ignore=[./README.md, ./node_modules]']
-  mockArgv(args)
+beforeEach(() => {
+  process.argv = oldArgs
+})
 
-  const mockExit = mockProcess.mockConsoleLog()
+test('When a project convention is kebabCase and all files are correct then we display a success message', async () => {
+  const args = ['path=./', 'ignore=[README.md, Dockerfile]']
+  mockArgv(args)
+  const mockLog = mockProcess.mockConsoleLog()
 
   await validate()
-  expect(mockExit).toHaveBeenCalled()
+  expect(mockLog).toHaveBeenCalledWith('Great, everything looks fine :)')
+})
+
+test('When a project convention is kebabCase and some files are wrong then we display an error message and kill a process', async () => {
+  const mockLog = mockProcess.mockConsoleLog()
+  const mockExit = mockProcess.mockProcessExit()
+
+  await validate()
+  expect(mockExit).toHaveBeenCalledWith(1)
+  expect(mockLog).toHaveBeenCalledWith(
+    'Uuu, some files are wrong. Please take a look on below files',
+    ['README.md']
+  )
+})
+
+test('When a project convention is camelCase and some files are wrong then we display an error message and kill a process', async () => {
+  const args = ['type=camelCase', 'ignore=[README.md]']
+  mockArgv(args)
+  const mockLog = mockProcess.mockConsoleLog()
+  const mockExit = mockProcess.mockProcessExit()
+
+  await validate()
+  expect(mockExit).toHaveBeenCalledWith(1)
+  expect(mockLog).toHaveBeenCalledWith(
+    'Uuu, some files are wrong. Please take a look on below files',
+    ['package-lock.json']
+  )
+})
+
+test('When a project convention is not supported then we display an error message and kill a process', async () => {
+  const args = ['type=newCase']
+  mockArgv(args)
+  const mockLog = mockProcess.mockConsoleLog()
+  const mockExit = mockProcess.mockProcessExit()
+
+  await validate()
+  expect(mockExit).toHaveBeenCalledWith(1)
+  expect(mockLog).toHaveBeenCalledWith('Uuu, we do not support newCase')
 })
--- src/validate.ts
+++ src/validate.ts
@@ -1,17 +1,41 @@
 import * as find from 'find'
 import * as lodash from 'lodash'
 
-const ignores = ['node_modules', '.git']
+const defaultIgnores = ['node_modules', '.git', 'idea']
 
 export async function validate(): Promise<void> {
-  const { path, type } = getSettings()
-  const files = await getFiles(path.toString())
-
-  const filtered = files.filter(file => !ignores.find(ignore => file.includes(ignore)))
+  const { path = './', type = 'kebabCase', ignore = [] } = getSettings()
   const validateFunction = lodash[type.toString()]
 
-  const withWrongTypes = filtered.filter(e => validateFunction(e) !== e)
-  console.log(withWrongTypes)
+  if (!validateFunction) {
+    console.log(`Uuu, we do not support ${type}`)
+    process.exit(1)
+    return
+  }
+
+  const files = await getFiles(path.toString())
+  const ignores = [...defaultIgnores, ...ignore]
+
+  const elementsToCompare = files
+    .filter(file => !ignores.find(ignore => file.includes(ignore)))
+    .map(file => {
+      return {
+        originalPath: file,
+        toCompare: file.replace(/\./g, '').split('/')
+      }
+    })
+
+  const elementsWithWrongValues = elementsToCompare.filter(e =>
+    e.toCompare.find(e => validateFunction(e) !== e)
+  )
+
+  if (!elementsWithWrongValues.length) {
+    console.log('Great, everything looks fine :)')
+  } else {
+    const paths = elementsWithWrongValues.map(e => e.originalPath)
+    console.log('Uuu, some files are wrong. Please take a look on below files', paths)
+    process.exit(1)
+  }
 }
 
 function getFiles(path: string): Promise<string[]> {
@@ -31,6 +55,7 @@ function getSettings() {
 }
 
 function parseToObject(args: string) {
+  if (!args) return {}
   const [key, val] = args.split('=')
   return {
     [key]: val.includes('[') ? parseToArray(val) : val
