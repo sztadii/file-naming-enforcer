fa659ec9c5e14c764f66ebabe44d6bbb35fdf85b
Krystian Sztadhaus <sztadii@gmail.com>
2020-06-04 01:00:24 +0400

    Wrap tests with describe

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -1,61 +1,63 @@
 const { execute } = require('@getvim/execute')
 
-test('When a project convention is kebabCase and all files are correct then we display a success message', async () => {
-  const stdout = await execute(
-    'npx ts-node src/index folder=./mocks ignore=[README.md, Dockerfile]'
-  )
-  expect(stdout).toBe('Great, everything looks fine :)')
-})
-
-test('When a project convention is kebabCase and some files are wrong then we display an error message and kill a process', async () => {
-  try {
-    await execute('npx ts-node src/index folder=./mocks')
-  } catch (e) {
-    const { stdout } = e
-    expect(stdout).toBe(
-      `Uuu, some files are not following your project naming convention (kebabCase). Please take a look on below files [ 'mocks/README.md' ]`
+describe('validate function', () => {
+  it('when a project convention is kebabCase and all files are correct then we display a success message', async () => {
+    const stdout = await execute(
+      'npx ts-node src/index folder=./mocks ignore=[README.md, Dockerfile]'
     )
-  }
+    expect(stdout).toBe('Great, everything looks fine :)')
+  })
 
-  expect.assertions(1)
-})
+  it('when a project convention is kebabCase and some files are wrong then we display an error message and kill a process', async () => {
+    try {
+      await execute('npx ts-node src/index folder=./mocks')
+    } catch (e) {
+      const { stdout } = e
+      expect(stdout).toBe(
+        `Uuu, some files are not following your project naming convention (kebabCase). Please take a look on below files [ 'mocks/README.md' ]`
+      )
+    }
 
-test('When a project convention is capitalize and some files are wrong then we display an error message and kill a process', async () => {
-  try {
-    await execute('npx ts-node src/index folder=./mocks type=capitalize ignore=[README.md]')
-  } catch (e) {
-    const { stdout } = e
-    expect(stdout).toBe(
-      `Uuu, some files are not following your project naming convention (capitalize). Please take a look on below files [ 'mocks/simple-js-file.js', 'mocks/some-scss-file.sass' ]`
-    )
-  }
+    expect.assertions(1)
+  })
 
-  expect.assertions(1)
-})
+  it('when a project convention is capitalize and some files are wrong then we display an error message and kill a process', async () => {
+    try {
+      await execute('npx ts-node src/index folder=./mocks type=capitalize ignore=[README.md]')
+    } catch (e) {
+      const { stdout } = e
+      expect(stdout).toBe(
+        `Uuu, some files are not following your project naming convention (capitalize). Please take a look on below files [ 'mocks/simple-js-file.js', 'mocks/some-scss-file.sass' ]`
+      )
+    }
 
-test('When a project convention is not supported then we display an error message and kill a process', async () => {
-  try {
-    await execute('npx ts-node src/index folder=./mocks type=newCase')
-  } catch (e) {
-    const { stdout } = e
-    expect(stdout).toBe('Uuu, we do not support newCase')
-  }
+    expect.assertions(1)
+  })
 
-  expect.assertions(1)
-})
+  it('when a project convention is not supported then we display an error message and kill a process', async () => {
+    try {
+      await execute('npx ts-node src/index folder=./mocks type=newCase')
+    } catch (e) {
+      const { stdout } = e
+      expect(stdout).toBe('Uuu, we do not support newCase')
+    }
 
-test('When a folder is empty then we display a error message and kill a process', async () => {
-  try {
-    await execute('npx ts-node src/index folder=./xxx')
-  } catch (e) {
-    const { stdout } = e
-    expect(stdout).toBe('Uuu, folder ./xxx is empty, please take a look on that')
-  }
+    expect.assertions(1)
+  })
 
-  expect.assertions(1)
-})
+  it('when a folder is empty then we display a error message and kill a process', async () => {
+    try {
+      await execute('npx ts-node src/index folder=./xxx')
+    } catch (e) {
+      const { stdout } = e
+      expect(stdout).toBe('Uuu, folder ./xxx is empty, please take a look on that')
+    }
 
-test('When a searched files are correct then we display a success message', async () => {
-  const stdout = await execute('npx ts-node src/index folder=./mocks ext=scss')
-  expect(stdout).toBe('Great, everything looks fine :)')
+    expect.assertions(1)
+  })
+
+  it('when a searched files are correct then we display a success message', async () => {
+    const stdout = await execute('npx ts-node src/index folder=./mocks ext=scss')
+    expect(stdout).toBe('Great, everything looks fine :)')
+  })
 })
